3a55d39a7be87d5394aadfdb722e77a8cba6ac65
Sapehiasahil1 <officialsahilsapehia03@gmail.com>
2023-10-14 12:37:47 +0530

    Done CheckBox

--- app/src/main/java/com/example/ux_material3/MainActivity.kt
+++ app/src/main/java/com/example/ux_material3/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.ux_material3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ux_material3.ui.theme.UXMaterial3Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,4 +34,105 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+data class ToggleableInfo(
+
+    val isChecked : Boolean,
+    val text : String
+)
+@Composable
+private fun CheckBoxes() {
+
+    val checkboxes = remember {
+        mutableListOf(
+            ToggleableInfo(
+                isChecked = false,
+                text = "Photos"
+            ),ToggleableInfo(
+                isChecked = false,
+                text = "Videos"
+            ),ToggleableInfo(
+                isChecked = false,
+                text = "Audio"
+            ),
+        )
+    }
+
+    var triState by remember {
+        mutableStateOf(ToggleableState.Indeterminate)
+    }
+
+    val toggleTriState = {
+        triState = when(triState) {
+            ToggleableState.Indeterminate -> ToggleableState.On
+            ToggleableState.On -> ToggleableState.Off
+            else -> ToggleableState.On
+        }
+
+        checkboxes.indices.forEach { index ->
+            checkboxes[index] = checkboxes[index].copy(
+                isChecked = triState == ToggleableState.On
+            )
+        }
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable {
+                toggleTriState()
+            }
+            .padding(end = 16.dp)
+    ) {
+        TriStateCheckbox(
+            state = triState,
+            onClick = toggleTriState
+        )
+        Text(text = "File Types")
+    }
+
+    checkboxes.forEachIndexed { index, info ->
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(start = 32.dp)
+                .clickable {
+                    checkboxes[index] = info.copy(
+                        isChecked = !info.isChecked
+                    )
+                }
+                .padding(end = 16.dp)
+        ) {
+            Checkbox(
+                checked = info.isChecked ,
+                onCheckedChange = { isChecked ->
+                    checkboxes[index] = info.copy(
+                        isChecked = isChecked
+                    )
+
+                }
+            )
+            Text(text = info.text)
+        }
+    }
+}
+
+@Composable
+fun Check() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding()
+    ) {
+            CheckBoxes()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFun() {
+    Check()
 }
